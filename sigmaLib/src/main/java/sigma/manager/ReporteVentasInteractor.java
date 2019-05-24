39576ca22a928249b344a1359fe0919b87acd14d
Mattencio <mapc.1993@hotmail.com>
2019-05-24 16:41:17 -0500

    Update ReporteVentasInteractor.java
    Filtrado reporte diario por código de operación

--- sigmaLib/src/main/java/sigma/manager/ReporteVentasInteractor.java
+++ sigmaLib/src/main/java/sigma/manager/ReporteVentasInteractor.java
@@ -18,7 +18,7 @@ public final class ReporteVentasInteractor {
         selectionMap = new HashMap<>();
         selectionMap.put(TipoReporte.REPORTE_VENTAS, TransaccionesEntry.COLUMN_MEDIO_PAGO+"!=0");
         selectionMap.put(TipoReporte.REPORTE_PCI, TransaccionesEntry.COLUMN_MEDIO_PAGO+"=?");
-        selectionMap.put(TipoReporte.REPORTE_PCI_DIA, TransaccionesEntry.COLUMN_MEDIO_PAGO+"=? AND " + TransaccionesEntry.COLUMN_TURNO + "=?");
+        selectionMap.put(TipoReporte.REPORTE_PCI_DIA, TransaccionesEntry.COLUMN_MEDIO_PAGO+"=? AND " + TransaccionesEntry.COLUMN_TURNO + "=? AND " + TransaccionesEntry.COLUMN_CODIGO_OPER + "='V'");
     }
 
     public enum TipoReporte{
