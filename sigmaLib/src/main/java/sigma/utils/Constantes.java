7cdd970ff0ffd4fbee287caf0eb5fcce13e67732
Mattencio <mapc.1993@hotmail.com>
2019-10-08 09:21:32 -0500

    Update Constantes.java
    Se removieron constantes que no perteneces al módulo de Sigma

--- sigmaLib/src/main/java/sigma/utils/Constantes.java
+++ sigmaLib/src/main/java/sigma/utils/Constantes.java
@@ -1,70 +1,11 @@
 package sigma.utils;
 
-import android.os.Environment;
-
 public final class Constantes {
-
-    public static final String UPDATE_APK = "UPDATE_APK";
-    public static final String ID_APP = "ID_APP";
-    public static final int TIEMPO_ENTRE_CLICKS = 1000;
-    public static final String VERSION_DATA_BASE = "1";
-    public static final String SQL_CONNECTOR = "jdbc:sqlite3://";
-    public static final int DECIMALES_INICIALES = 2;
 
     //---------------SharedPreferences-------------------------------------
     public static final String PREFERENCE_SETTINGS = "PREFERENCE_SETTINGS";
 
-    //---------------Timers ---------------------------------------------//
-    public static final int TIEMPO_TRANSICIONES = 300;
-    public static final int TIEMPO_CARGA_FRAGMENTS = 600;
-    public static final int TIEMPO_INACTIVIDAD_DEFAULT = 180000;
-
-    public static final String REGISTRO_INTERNO = "REGISTRO_INTERNO";
-    public static final String REGISTRO_EXTERNO = "REGISTRO_EXTERNO";
-    public static final String COLOR_APP_AZUL = "AZUL";
-
-    //----------------------Tipo Operacion--------------------------------
-    public static final String VENTA = "V"; //valor de prueba
-    public static final String DEVOLUCION = "D"; //valor de prueba
-    public static final String CONSULTA_X = "X"; //valor de prueba
-    public static final String CONSULTA_Z = "Z"; //valor de prueba
-    public static final String SINCRONIZACION  = "J"; //valor de prueba
-    public static final String CIERRE = "CIERRE";
-
-    //------------------------Impresora-------------------------------------
-
-
-
-    public static final String TICKET_SMALL = "¸";
-    public static final String TICKET_NORMAL = "‗";
-    public static final String TICKET_MEDIUM = "░";
-    public static final String TICKET_LARGE = "▓";
-
-
-    public static final int NOPAPER = 3;
-    public static final int LOWBATTERY = 4;
-    public static final int PRINTVERSION = 5;
-    public static final int PRINTBARCODE = 6;
-    public static final int PRINTQRCODE = 7;
-    public static final int PRINTPAPERWALK = 8;
-    public static final int PRINTFORMATTEXT = 9;
-    public static final int PRINTTEXT = 17;
-    public static final int CANCELPROMPT = 10;
-    public static final int PRINTERR = 11;
-    public static final int OVERHEAT = 12;
-    public static final int PRINTPICTURE = 14;
-    public static final int NOBLACKBLOCK = 15;
-    public static final int PRINTPICTURECONTENT = 16;
-
-    public static final boolean LAUNCHER = true;
-
-    //------------------------Update apk-----------------------
-    public static final String PATH_APK = Environment.getExternalStorageDirectory() + "/Download/";
-    public static final String NOMBRE_APK = "apk_update.apk";
-    public static final String ACTION_UPDATE_END = "com.pagatodoholdings.posandroid.UPDATE_END";
-
     public static final String BCODE_FIELD = "C<";
-
 
     public enum Preferencia {
         ESTADO_REGISTRO,
@@ -110,7 +51,6 @@ public final class Constantes {
         ENVIADO,
         REGISTRADO
     }
-
 
     private Constantes() {
     }
